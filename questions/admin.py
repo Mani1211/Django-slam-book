ade0dfa529aeca3ba304b66176b3f56035187c7d
Mani1211 <smanivasagamtnj@gmail.com>
2020-04-26 02:40:04 +0530

    Admin updated

--- questions/admin.py
+++ questions/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Questions
 
 # Register your models here.
-admin.site.register(Questions)
+
+class QuestionsAdmin(admin.ModelAdmin):
+    list_display=('id','name','number','weakness','advice','positive','like','words','memory')
+admin.site.register(Questions,QuestionsAdmin)
