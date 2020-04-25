a08765b47183b6befac49e928661f522b2980502
Mani1211 <smanivasagamtnj@gmail.com>
2020-04-25 20:01:48 +0530

    error resolved

--- slam_book/settings.py
+++ slam_book/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'] = dj_database_url.update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT =  os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
