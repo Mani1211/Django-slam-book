cbba882c7b3729dad3987728d566ec7ca9c5afcf
Mani1211 <smanivasagamtnj@gmail.com>
2020-04-25 18:48:32 +0530

    update

--- slam_book/settings.py
+++ slam_book/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'ulp^1cv-j26n28-h1g7a2m-^cj!wdl=rk4gn&@vn%49kfh&na7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://mani-slam-book.herokuapp.com/','127.0.0.1']
+# ALLOWED_HOSTS = ['https://mani-slam-book.herokuapp.com/','127.0.0.1:8000']
+ALLOWED_HOSTS=[]
 
 
 # Application definition
@@ -123,8 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =  os.path.join(BASE_DIR,'static')
-
+STATIC_ROOT =  os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
