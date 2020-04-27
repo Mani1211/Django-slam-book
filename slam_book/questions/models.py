224a9e0f69454b51765a4a5d6ab61ab05752eac3
Mani1211 <smanivasagamtnj@gmail.com>
2020-04-28 00:01:28 +0530

    Number Field Updated

---
+++ slam_book/questions/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Questions(models.Model):
+
+    name  = models.CharField(max_length=255)
+    number = models.CharField(max_length=10)
+    advice = models.CharField(max_length=255)
+    positive = models.CharField(max_length=255)
+    weakness = models.CharField(max_length=255)
+    like = models.CharField(max_length=255)
+    words = models.CharField(max_length=255)
+    memory = models.CharField(max_length=255)
+   
+    def __str__(self):
+        return self.name
